2b38cb0811864022e4149998cbea466457160eca
Brendan Hennessy <bhennes2@gmail.com>
2012-02-16 08:44:53 -0600

    Removed default Rails page

--- config/routes.rb
+++ config/routes.rb
@@ -56,7 +56,12 @@ SampleApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  
+  root :to => "pages#home"
+  
+  match '/contact', :to => 'pages#contact'
+  match '/about', :to => 'pages#about'
+  match '/help', :to => 'pages#help'
 
   # See how all your routes lay out with "rake routes"
 
